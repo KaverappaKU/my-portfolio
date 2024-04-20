4b711aac733d0fd449c635ce254200f6091e84fc
Kaverappa KU <caakash357@gmail.com>
2024-04-19 23:05:45 -0400

    Added mailto

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,57 +1,65 @@
-import { useEffect } from "react";
 import React from "react";
 import "./contact.css";
 
 export default function Contact() {
-  useEffect(() => {});
   return (
     <div>
-      <section class="contact-me" id="contact-me">
-        <div class="contact-heading">
+      <section className="contact-me" id="contact-me">
+        <div className="contact-heading">
           <h1>Let's get in touch</h1>
         </div>
-        <div class="contact-form">
-          <div class="fname">
-            <label for="fname">First Name</label>
-            <input
-              type="text"
-              id="fname"
-              name="fname"
-              placeholder="Enter your First Name"
-            />
+        <form
+          action="mailto:caakash357@gmail.com"
+          method="post"
+          enctype="text/plain"
+        >
+          <div className="contact-form">
+            <div className="fname">
+              <label htmlFor="fname">First Name</label>
+              <input
+                type="text"
+                id="fname"
+                name="fname"
+                placeholder="Enter your First Name"
+                required
+              />
+            </div>
+            <div className="lname">
+              <label htmlFor="lname">Last Name</label>
+              <input
+                type="text"
+                id="lname"
+                name="lname"
+                placeholder="Enter your Last Name"
+                required
+              />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your Email"
+                required
+              />
+            </div>
+            <div className="message">
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Enter your Message"
+                required
+              ></textarea>
+            </div>
           </div>
-          <div class="lname">
-            <label for="lname">Last Name</label>
-            <input
-              type="text"
-              id="lname"
-              name="lname"
-              placeholder="Enter your Last Name"
-            />
+          <div className="send-button">
+            <button className="submit" type="submit">
+              Send
+            </button>
           </div>
-          <div class="email">
-            <label for="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="Enter your Email"
-            />
-          </div>
-          <div class="message">
-            <label id="message">Message</label>
-            <textarea
-              id="messages"
-              name="message"
-              placeholder="Enter your Message"
-            ></textarea>
-          </div>
-        </div>
-        <div class="send-button">
-          <button class="submit" type="submit">
-            Send
-          </button>
-        </div>
+        </form>
       </section>
     </div>
   );
